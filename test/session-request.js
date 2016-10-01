ac12fab4b57da6007cf9adb9e58b5339c98b185d
techird <techird@qq.com>
2016-10-01 17:21:44 +0800

    add test

--- test/session-request.js
+++ test/session-request.js
@@ -1,30 +1,38 @@
-var should = require('should');
-var sinon = require('sinon');
+'use strict';
+
+const should = require('should');
+const sinon = require('sinon');
 require('should-sinon');
-var request = require('../lib/session-request').request;
+const request = require('../lib/session-request').request;
 
 describe('Session Request', function() {
+    let wx;
+    beforeEach(function() {
+        wx = global.wx = {
+            login() {},
+            request() {}
+        };
+    });
+
     it('should be a function', function() {
         should(request).be.a.Function();
     });
+
     it('should throw an error when no options passed', function() {
         should.throws(function() {
             request();
         })
     });
+
     it('should call login for the first request', function() {
-        var wx = global.wx = {
-            login() {},
-            request() {}
-        };
         sinon.stub(wx, 'login');
-        sinon.stub(wx, 'request');
         request({
             url: 'https://www.mydomain.com/ajax/action'
         });
         wx.login.should.be.calledOnce();
     });
-    it('should call with code for the first request', function() {
 
+    it('should call with code for the first request', function() {
+        
     });
 });
