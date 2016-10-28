68b2f6bb63d4c7745322594d539e08d23ed1940b
miusuncle <miusuncle@gmail.com>
2016-10-29 00:06:49 +0800

    mock `wx.removeStorageSync`

--- test/helpers/setupFakeWxAPI.js
+++ test/helpers/setupFakeWxAPI.js
@@ -4,6 +4,7 @@ function setupFakeWxAPI() {
 
     wx.setStorageSync = (key, name) => storage[key] = name;
     wx.getStorageSync = key => storage[key];
+    wx.removeStorageSync = key => delete storage[key];
     wx.login = () => void(0);
     wx.getUserInfo = () => void(0);
     wx.checkSession = () => void(0);
